134dbeaacedff9b428263d8b45a277e962bc680f
UrielRojas <adur987@gmail.com>
2026-09-23 19:48:08 -0500

    nilson medina// Funcion de butoon en home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+
+  // Función para hacer scroll suave hacia la sección de servicios
+  scrollAServicios() {
+    const seccion = document.getElementById('servicios');
+    if (seccion) {
+      seccion.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
